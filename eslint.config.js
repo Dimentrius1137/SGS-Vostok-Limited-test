f4e806d3c4551222256569ad1f27e8fa7b00f34a
Dimentrius <lavrentevd579@gmail.com>
2025-04-09 00:02:56 +0300

    init project

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'vue/html-self-closing': 'off',
+      'vue/html-indent': ["warn", "tab"],
+      'vue/attribute-hyphenation': 'off'
+    },
+  },
+])
