0695366f39f6adc4717e991b662954630a3c7946
Dimentrius <lavrentevd579@gmail.com>
2025-04-12 08:11:28 +0300

    add form components

---
+++ src/features/submit-form/ui/DividerLine.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="divider">
+		<div class="divider__line"></div>
+	</div>
+</template>
+<script setup></script>
+<style scoped lang="scss">
+	.divider{
+		height: 50px;
+		align-content: center;
+		&__line{
+			background-color: var(--gray);
+			width: 1px;
+			height: 70%;
+		}
+		@media all and (max-width: 800px) {
+			display: none;
+		}
+	}
+
+</style>
--- src/pages/ResultPage.vue
+++ src/pages/ResultPage.vue
@@ -1,10 +1,39 @@
 <template>
-	<div></div>
+	<div class="result-page">
+		<RouterLink
+			class="result-link"
+			to="/form"
+		>
+			<- К форме
+		</RouterLink>
+		<h1 class="result-page__title">
+			Заявка
+		</h1>
+		<div class="application">
+		</div>
+	</div>
 </template>
 
 <script setup>
+
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+	.result-page{
+		&__title{
+			text-align: center;
+			margin-top: 60px;
+			margin-bottom: 10px;
+		}
+		.result-link{
+			position: fixed;
+			left: 100px;
+			top: 45px;
+			color: var(--gray);
+			@media all and (max-width: 800px) {
+				top: 20px;
+				left: 20px;
+			}
+		}
+	}
 </style>
